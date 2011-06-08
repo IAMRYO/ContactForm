26432abb775a2025341c4695e586d8b2a44b0fe9
Ryan Clark <rclark.sf@gmail.com>
2011-06-08 23:55:38 +0000

    pre-test

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php session_start() ?>
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]--><!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]--><!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]--><!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]--><!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]--><head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<link rel="stylesheet" href="css/style.css?v=2">
+
+	<script src="js/libs/modernizr-1.7.min.js"></script></head>
+<body>
+	<div id="contact-form" class="clearfix">
+		<h1>Get in Touch!</h1>
+		<h2>Fill out our super-swanky HTML5 contact form below to get in touch with us! Please provide as much information as possible for us to help with your inquiry. <img src="/wp-includes/images/smilies/icon_smile.gif" alt=":)" class="wp-smiley"> </h2>
+		
+		<?php
+		// init variables
+		$cf = array();
+		$sf = false;
+
+		if(isset($_SESSION['cf_returndata'])){
+			$cf = $SESSION['cf_returndata'];
+			$sr = true;
+		}
+		?>
+
+		<ul id="errors" class="<?php echo ($sr && !$cf['form_ok']) ? 'visibe' : ''; ?>">
+			<li id="info">There were some problems with your form submission:</li>
+			<?php
+			if(isset($cf['errors']) && count($cf['errors']) > 0 :
+				foreach($cf['errors'] as $error) :
+			?>
+			<li><?php echo $error ?></li>
+			<?php
+				endforeach;
+			endif;
+			?>		
+		</ul>
+		<p id="success" class="<?php echo ($sr && $cf['form_ok']) ? 'visible' : ''; ?>">Thanks for your message! We will get back to you ASAP!</p>
+		<form method="post" action="process.php">
+			<label for="name">Name: <span class="required">*</span></label>
+			<input type="text" id="name" name="name" value="" placeholder="First Last" required="required" autofocus="autofocus" />
+			
+			<label for="email">Email Adress: <span class="required">*</span></label>
+			<input type="email" id="email" name="email" value="" placeholder="YourName@example.com" required="required" />
+
+			<label for="telephone">Telephone: </label>
+			<input type="tel" id="telephone" name="telephone" value="" placeholder="(555)555-5555" />
+
+			<label for="inquiry">Inquiry: </label>
+			<select id="inquiry" name="inquiry">
+				<option value="general">General</option>
+				<option value="sales">Sales</option>
+				<option value="support">Support</option>
+			</select>
+
+			<label for="message">Message: <span class="required">*</span></label>
+			<textarea id="message" name="message" placeholder="Your message must be greater than 20 characters." required="required" data-minlength="20">
+			</textarea>
+
+			<span id="loading"></span>
+			<input type="submit" value="Cheers!" id="submit-button" />
+			<p id="req-field-desc"><span class="required">*
+		</span> indicates a required field.</p>
+			</form>
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+	<script>!window.jQuery && document.write(unescape('%3Cscript src="js/libs/jquery-1.5.1.min.js"%3E%3C/script%3E'))</script>
+	<script src="js/plugins.js"></script>
+	<script src="js/script.js"></script>
+	<!--[if lt IE 7 ]>
+	<script src="js/libs/dd_belatedpng.js"></script>
+	<script> DD_belatedPNG.fix('img, .png_bg');</script>
+	<![endif]--></body>
+</html>
